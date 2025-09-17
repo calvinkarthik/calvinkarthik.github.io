e24399f349b1a4ff01b8525694bb9f1ad706dfed
calvinkarthik <calvin.g.karthik@gmail.com>
2025-09-16 20:41:49 -0400

    Use root base for user site

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-website/', // matches your repo name
+  base: '/', // root
   plugins: [react()],
 })
