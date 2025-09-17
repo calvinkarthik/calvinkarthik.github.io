ef3293e649c8ff9fd5e56fe2e869dba441749670
calvinkarthik <calvin.g.karthik@gmail.com>
2025-09-16 20:52:34 -0400

    chore: use root base for user site attempt 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // root
+  base: '/', // root for deployment
   plugins: [react()],
 })
